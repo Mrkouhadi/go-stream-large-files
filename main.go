b555d2193520799db74d6d8450e36eeff38510ca
mrkouhadi <teacherbryan@outlook.com>
2024-01-02 22:06:15 +0800

    feat: added profiling

--- main.go
+++ main.go
@@ -1,12 +1,21 @@
 package main
 
-import "time"
+import (
+	"time"
+
+	"github.com/pkg/profile"
+)
 
 func main() {
+
+	// profiling our program:
+	defer profile.Start(profile.MemProfileRate(1), profile.MemProfile, profile.ProfilePath(".")).Stop()
+	// wait 3 seconds then start sending the file
 	go func() {
-		time.Sleep(time.Second * 4)
-		SendFile(1000000000)
+		time.Sleep(time.Second * 3)
+		// SendFile(10000)
 	}()
 	server := &FilServer{}
 	server.Start()
+
 }
